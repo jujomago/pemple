617d14d1fca1b9c8f6b0efc3a449bae73df12c2d
Josue Mancilla <jujomago@gmail.com>
2012-07-18 18:57:56 -0400

    mas escalofrios
    a minutos de salir

---
+++ application/controllers/acceso.php
@@ -0,0 +1,68 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+
+class Acceso extends CI_Controller {
+
+	var $loguin=FALSE;
+
+	function Acceso(){
+		parent::__construct();		
+	} 
+	function prueba(){
+	/*	$data['titulo']="esto es una pruba";
+		$this->load->view('header',$data);		
+		*/	
+	//	$this->load->view('template');
+
+		//$this->load->view('template',$data);*/
+	//	$this->template->set('titulo','Este es el titulo');
+	//	$this->template->load('template','header',);
+	
+	 	$this->template->set('titulo',"BLA BLA");
+	 	$this->template->set('otro','este es el valor de otro');
+		$this->template->set('libjs',"http://google.com/pruebita.js");
+		$reqjs=array('libreria1.js','libreria2.js','libreria3.js');
+		$this->template->set('reqjs',$reqjs);
+		$this->template->view('about',array('info_alert'=>'	Welcome to the free MediaLoot admin panel template, this could be an informative message.'));
+		
+	}
+	public function index($msg="ok")
+	{
+		if($msg=='error')
+			$data["mensaje"]="El usuario no existe";
+		else
+			$data=array();
+		$this->load->view('login',$data);		
+	}
+	public function is_loquegado(){
+		$this->loguin=$this->session->userdata('LOGUEADO');
+	}
+	public function login(){
+	 	$usuario=$this->input->post('usuario',true);
+	 	$clave=$this->input->post('clave',true);
+	 	$where=array(
+	 		'usuario'=>$usuario,
+	 		'password'=>$clave
+	 	);
+	 	$query=$this->db->get_where('usuarios',$where);
+	 	if($query->num_rows()==1){
+	 		$query2=$this->db->get_where('vusuario_persona',$where);
+	 		$datos_persona=$query2->row();
+	 		$this->session->set_userdata($datos_persona);
+	 		// print_r($this->session->all_userdata());s
+	 		$this->session->set_suserdata('LOGUEADO', TRUE);
+	 		redirect('/empresas/abm');
+	 	}else{
+	 		redirect('acceso/index/error');
+	 	}
+	}
+	public function logout(){
+		$this->session->sess_destroy();
+		$this->loguin=FALSE;
+		redirect('/acceso');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/wselcome.php */
--- application/views/header.php
+++ application/views/header.php
@@ -72,7 +72,7 @@
 	<header id="header">
 		<hgroup>
 			<h1 class="site_title"><a href="index.html">Proyecto empleabilidad</a></h1>
-			<h2 class="section_title">Dashboard</h2><div class="btn_view_site"><a href="http://www.medialoot.com">Ver el portal</a></div>
+			<h2 class="section_title"><?=$titulo?></h2><div class="btn_view_site"><a href="http://www.medialoot.com">Ver el portal</a></div>
 		</hgroup>
 	</header> <!-- end of header bar -->
 		<section id="secondary_bar">
